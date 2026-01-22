691523cc270acf4653824ff571d59ad7542551b4
AbhishekUS5 <abhishekshet12@gmail.com>
2026-01-22 21:45:30 +0530

    Recursion

--- Recursion/ex12.java
+++ Recursion/ex12.java
@@ -1,3 +1,13 @@
 public class ex12 {
-    
+    static void printNumber(int n){
+        if(n <= 0)
+            return;
+        System.out.println(n+ " ");
+        printNumber(n-1);
+    }
+    public static void main(String[] args) {
+        int n=5;
+        System.out.println("Output ");
+        printNumber(n);
+    }
 }
